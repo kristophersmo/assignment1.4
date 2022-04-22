749382ce14450b43e6c57d08f1e183c2ae567f8f
kristophersmo <103333739+kristophersmo@users.noreply.github.com>
2022-04-22 18:19:27 -0500

    program submission for assignment 3

---
+++ assignment 3/src/binary_to_hex.java
@@ -0,0 +1,35 @@
+// Binary to Hex conversion from user input
+// by Kristopher Smolarek (CSC250)
+
+import java.util.Scanner;
+
+public class binary_to_hex
+{
+	public static void main(String[] args)
+	{
+		String user_binary = getBinary();
+		System.out.println();
+		System.out.println("The binary input is: " + user_binary);
+		String Hex = hexConvert(user_binary);
+		userOutput(Hex);
+	}
+	static String getBinary()
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter a binary number: ");
+		String user_binary = input.nextLine();
+		input.close();
+		return user_binary;
+	}
+	static String hexConvert(String user_binary)
+	{
+		// convert user_binary to type int (number to convert, base)
+		int user_int = Integer.parseInt(user_binary, 2);
+		String Hex = Integer.toHexString(user_int);
+		return Hex;
+	}
+	static void userOutput(String Hex)
+	{
+		System.out.println("The hexadecimal equivilent is: " + Hex.toUpperCase());
+	}
+}
